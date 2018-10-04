29177a76dde954ace60225c79095ea8455d645e1
Marcial <marcialandres@me.com>
2018-10-04 17:29:16 -0400

    banching

--- src/components/Profile/ProfileV.js
+++ src/components/Profile/ProfileV.js
@@ -24,31 +24,69 @@ import profile_icon from '../../../assets/profile_icon.png';
 const ProfileV = (props) => {
     return (
       <Container style={{marginTop: 20}}>
-        <Header style = {{height: 55}}>
-          <Left></Left>
-          <Body style = {{height: 45}}>
-            <Title>PIQUIS</Title>
-          </Body>
-          <Right />
-        </Header>
-          <Content>
-              <View>
-                  <Text>Profile</Text>
-              </View>
-          </Content>
-          <Footer>
-            <Left>
-              <Button transparent>
-                <Image
-                  style = {{height: 38, width: 38}}
-                  source={require('../../../assets/profile_icon.png')}
-                  />
-              </Button>
-            </Left>
+        <View style={styles.MainContainer}>
+          <Header style = {{height: 55}}>
+            <Left></Left>
+            <Body style = {{height: 45}}>
+              <Title>PIQUIS</Title>
+            </Body>
             <Right></Right>
-          </Footer>
+          </Header>
+            <Content>
+                <View>
+                    <Text>Profile</Text>
+                </View>
+            </Content>
+            <Footer>
+              <Left>
+                <Button transparent>
+                  <Image
+                    style = {{height: 38, width: 38}}
+                    source={require('../../../assets/profile_icon.png')}
+                    />
+                </Button>
+              </Left>
+              <Right></Right>
+            </Footer>
+          </View>
       </Container>
     );
 }
 
 export default ProfileV;
+
+const styles = StyleSheet.create({
+    MainContainer: {
+      //flex: 1,
+      justifyContent: 'center',
+      alignItems: 'center',
+      position: 'absolute',
+      width: '100%',
+      height: '100%',
+      backgroundColor: '#EFB289',
+    },
+    wrapper: {
+      flex: 1,
+    },
+    container: {
+      flex: 1,
+      //alignItems: 'center',
+      // justifyContent: 'center',
+      // backgroundColor: '#2896d3',
+      // paddingLeft: 40,
+      // paddingRight: 40,
+    },
+    header: {
+      fontSize: 30,
+      marginBottom: 60,
+      color: 'gray',
+      fontWeight: 'bold',
+    },
+    profile_icon: {
+        width: 170,
+        height: 225,
+        //resizeMode: "contain",
+        alignSelf: 'center',
+        marginBottom: 55,
+    },
+  });
