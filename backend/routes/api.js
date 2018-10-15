703b264c0bcc290c7d61a43dd90175640fc2f779
Gabriel Fajardo <gfajardo95@gmail.com>
2018-10-15 11:05:53 -0400

    create restaurant routes

--- backend/routes/api.js
+++ backend/routes/api.js
@@ -2,7 +2,9 @@ const express = require('express'),
 router = express.Router();
 
 const dishRoutes = require('./dish');
+const restaurantRoutes = require('./restaurant');
 
-router.use('/dish', dishRoutes)
+router.use('/dish', dishRoutes);
+router.use('/restaurant', restaurantRoutes);
 
 module.exports = router;
