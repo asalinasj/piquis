c3505c8134e6d15c95b864faf8df7d9cd0632fd0
Gabriel Fajardo <gfajardo95@gmail.com>
2018-10-25 15:49:46 -0400

    create Rating model, route, and migration

--- backend/routes/api.js
+++ backend/routes/api.js
@@ -1,9 +1,11 @@
 const express = require('express'),
 router = express.Router();
 
+const ratingRoutes = require('./rating');
 const dishRoutes = require('./dish');
 const restaurantRoutes = require('./restaurant');
 
+router.use('/rating', ratingRoutes);
 router.use('/dish', dishRoutes);
 router.use('/restaurant', restaurantRoutes);
 
